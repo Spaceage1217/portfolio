611fe038860d2a9bb1e483f47648596d24cf32b1
Spaceage1217 <dimeji.faluyi@gmail.com>
2017-03-24 20:06:14 -0500

    MY FIRST COMMIT FOR PORTFOLIO WEBSITE

---
+++ scripts/index.js
@@ -0,0 +1,24 @@
+/**
+ * index.js
+ * - All our useful JS goes here, awesome!
+ */
+
+$(document).ready(function(){
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+});
+
+
+console.log("JavaScript is amazing!");
